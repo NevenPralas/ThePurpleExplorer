48cc01f5d1a2a63bf6fdca6359aa52e032aca575
NevenPralas <148341676+NevenPralas@users.noreply.github.com>
2023-11-03 17:24:10 +0100

    Memory 90% gotov
    Još je potrebno samo kada pobijedimo otvoriti škrinju, myb vremensko ograničenje postavit.

---
+++ Assets/Scripts/GlobalMemory.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalMemory : MonoBehaviour
+{
+    public static int svi=0;
+    public static int kangaroo=0;
+    public static int emu=0;
+    public static int kazuar=0;
+    public static int kljunas=0;
+    public static int koala=0;
+    public static int snake=0;
+    public static int tapir=0;
+    public static int tasman=0;
+    public static int pobjeda = 0; // pobjeda kada smo skupili 16!
+
+    public static GameObject pamtiButton;
+
+    public static GameObject pamti = null;
+}
