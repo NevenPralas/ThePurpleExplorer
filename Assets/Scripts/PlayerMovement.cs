cc66506b69e30053207a9a507a0ce17d8922e854
mikulickata <mikulic.katarina.mk@gmail.com>
2023-12-12 14:05:06 +0100

    Antarktika v1.0

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 700f;
+
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        movement.Normalize(); // Normalizacija vektora kako bi spriječili brže kretanje dijagonalno
+
+        // Rotacija igrača prema smjeru kretanja
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        // Pomak igrača
+        characterController.Move(movement * moveSpeed * Time.deltaTime);
+    }
+}
