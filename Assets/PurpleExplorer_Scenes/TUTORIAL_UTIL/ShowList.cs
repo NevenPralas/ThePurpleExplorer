986fd8e154ab3ae2885351c04d2a92389fe09947
Spider-Man-s <filip.posavec@fer.hr>
2023-12-22 01:21:51 +0100

    dodan grab dio
    treba jos dodati translaciju na penjanje i prilagodit funkcionalnost

---
+++ Assets/PurpleExplorer_Scenes/TUTORIAL_UTIL/ShowList.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowList : MonoBehaviour
+{
+    public List<GameObject> objectsToShow = new List<GameObject>();
+
+    public void ShowObjects()
+    {
+        foreach (GameObject obj in objectsToShow)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Object doesn't exist");
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ShowObjects();
+        }
+    }
+}
