7590eb74547234c12ae6c1ff962d80009661acb4
NevenPralas <148341676+NevenPralas@users.noreply.github.com>
2023-12-30 22:58:56 +0100

    novo, vjv najnovije
    ...

--- Assets/Scripts/Africa/Nestajanje.cs
+++ Assets/Scripts/Africa/Nestajanje.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Nestajanje : MonoBehaviour
 {
     public GameObject[] predmeti = new GameObject[12];
+    public TextMeshProUGUI tekstic;
+
+    bool jednom = false;
+
+    public GameObject button1;
+    public GameObject button2;
+
+    public GameObject plakat;
 
     // Update is called once per frame
     void Update()
@@ -12,12 +22,31 @@ public class Nestajanje : MonoBehaviour
         if (Pogodak.pogodjeno2 == 2)
         {
 
+            plakat.GetComponent<Canvas>().enabled = true;
+
+            if (GlobalMemory.totemAfrika == 0)
+            {
+                button1.SetActive(false);
+                button2.SetActive(false);
+            }
+            else
+            {
+                button1.SetActive(true);
+                button2.SetActive(true);
+            }
+
             for (int i = 0; i < predmeti.Length; i++)
             {
                 predmeti[i].GetComponent<MeshRenderer>().enabled = false;
                 predmeti[i].GetComponent<BoxCollider>().enabled = false;
             }
-
+            if (!jednom)
+            {
+                jednom = true;
+                tekstic.text = "Nice job";
+                tekstic.color = Color.green;
+                StartCoroutine(CekanjeText());
+            }
         }
         else if(Pogodak.pogodjeno2 != 2)
         {
@@ -29,6 +58,15 @@ public class Nestajanje : MonoBehaviour
                 }
             
         }
+    }
+
+    IEnumerator CekanjeText()
+    {
+
+        yield return new WaitForSeconds(3f);
+
+        tekstic.text = " ";
+
     }
 
 }
--- Assets/Scripts/Africa/SmanjivanjeLoptica.cs
+++ Assets/Scripts/Africa/SmanjivanjeLoptica.cs
@@ -11,35 +11,42 @@ public class SmanjivanjeLoptica : MonoBehaviour
 
     public static int poraz = 0;
 
+    public GameObject button;
+
+
     // Update is called once per frame
     void Update()
     {
-        if (BrojanjeLoptica.brojacLoptica == 19) tekstic.text = "19/20";
-        else if (BrojanjeLoptica.brojacLoptica == 18) tekstic.text = "18/20";
-        else if (BrojanjeLoptica.brojacLoptica == 17) tekstic.text = "17/20";
-        else if (BrojanjeLoptica.brojacLoptica == 16) tekstic.text = "16/20";
-        else if (BrojanjeLoptica.brojacLoptica == 15) tekstic.text = "15/20";
-        else if (BrojanjeLoptica.brojacLoptica == 14) tekstic.text = "14/20";
-        else if (BrojanjeLoptica.brojacLoptica == 13) tekstic.text = "13/20";
-        else if (BrojanjeLoptica.brojacLoptica == 12) tekstic.text = "12/20";
-        else if (BrojanjeLoptica.brojacLoptica == 11) tekstic.text = "11/20";
-        else if (BrojanjeLoptica.brojacLoptica == 10) tekstic.text = "10/20";
-        else if (BrojanjeLoptica.brojacLoptica == 9) tekstic.text = "9/20";
-        else if (BrojanjeLoptica.brojacLoptica == 8) tekstic.text = "8/20";
-        else if (BrojanjeLoptica.brojacLoptica == 7) tekstic.text = "7/20";
-        else if (BrojanjeLoptica.brojacLoptica == 6) tekstic.text = "6/20";
-        else if (BrojanjeLoptica.brojacLoptica == 5) tekstic.text = "5/20";
-        else if (BrojanjeLoptica.brojacLoptica == 4) tekstic.text = "4/20";
-        else if (BrojanjeLoptica.brojacLoptica == 3) tekstic.text = "3/20";
-        else if (BrojanjeLoptica.brojacLoptica == 2) tekstic.text = "2/20";
-        else if (BrojanjeLoptica.brojacLoptica == 1) tekstic.text = "1/20";
-        else if (BrojanjeLoptica.brojacLoptica == 0) {
-            tekstic.text = "0/20";
-            if (Pogodak.pogodjeno2!=2) {
+        if(BrojanjeLoptica.brojacLoptica == 20) {
+            button.SetActive(true); }
+        else if (BrojanjeLoptica.brojacLoptica == 19) {
+            button.SetActive(false); tekstic.text = "19/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 18) { button.SetActive(false); tekstic.text = "18/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 17) { button.SetActive(false); tekstic.text = "17/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 16) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "16/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 15) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "15/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 14) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "14/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 13) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "13/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 12) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "12/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 11) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "11/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 10) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "10/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 9) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "9/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 8) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "8/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 7) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "7/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 6) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "6/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 5) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "5/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 4) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "4/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 3) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "3/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 2) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "2/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 1) { button.SetActive(false); if (Pogodak.pogodjeno2 != 2) tekstic.text = "1/20"; }
+        else if (BrojanjeLoptica.brojacLoptica == 0)
+        {
+            button.SetActive(false);
+            if (Pogodak.pogodjeno2 != 2)
+            {
+                tekstic.text = "0/20";
                 poraz = 1;
 
-                Pogodak.pogodjeno = 0;
-                Pogodak.pogodjeno2 = 0;
 
                 StartCoroutine(CekanjeFunkcija());
             }
@@ -51,6 +58,14 @@ public class SmanjivanjeLoptica : MonoBehaviour
 
         yield return new WaitForSeconds(5f);
 
-        SceneManager.LoadScene("Otok");
+        if (Pogodak.pogodjeno2 != 2)
+        {
+            Pogodak.pogodjeno = 0;
+            Pogodak.pogodjeno2 = 0;
+            SceneManager.LoadScene("Otok");
+        }
+        
     }
+
+
 }
